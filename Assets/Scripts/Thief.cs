7188c49eda0852fde3c13c9ef7534eb02bf9839e
amattakarinntou <snkarin@nike.eonet.ne.jp>
2025-08-29 14:46:02 +0900

    横移動改善
    横移動を改善した

--- Assets/Scripts/Thief.cs
+++ Assets/Scripts/Thief.cs
@@ -10,45 +10,51 @@ public class Thief : MonoBehaviour
     public int maxJumpCount = 1;
     public int jumpCount = 0;
 
+    private float moveDirection = 0f;
+    public float moveSpeed = 3f;
+    public float jumpPower = 5f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
-    void Start()
-    {
-        hp = new Hp(3);
-        rigidBody = GetComponent<Rigidbody>();
-        thiefInput = new ThiefInput();
-        thiefInput.Move.MoveRight.started += MoveRight;
-        thiefInput.Move.MoveLeft.started += MoveLeft;
-        thiefInput.Move.Jump.started += Jump;
-        thiefInput.Enable();
-    }
+     void Start()
+     {
+         hp = new Hp(3);
+         rigidBody = GetComponent<Rigidbody>();
+         thiefInput = new ThiefInput();
+         //Dで右移動
+         thiefInput.Move.MoveRight.performed += ctx => moveDirection = 1f;
+        thiefInput.Move.MoveRight.canceled += ctx =>
+        {
+            if (moveDirection == 1f) moveDirection = 0f;
+        };
+         //Aで左移動
+         thiefInput.Move.MoveLeft.performed += ctx => moveDirection = -1f;
 
-    // Update is called once per frame
-    void Update()
-    {
-       
-    }
+        thiefInput.Move.MoveLeft.canceled += ctx =>
+        {
+            if (moveDirection == -1f) moveDirection = 0f;
+        };
+         //ジャンプ
+         thiefInput.Move.Jump.started += Jump;
+         thiefInput.Enable();
+     }
 
-    //Dで右移動
-    void MoveRight(InputAction.CallbackContext context)
-    {
-        rigidBody.linearVelocity += new Vector3(1, 0, 0);
-        Debug.Log("D");
-    }
-
-    //Aで左移動
-    void MoveLeft(InputAction.CallbackContext context) 
-    {
-        rigidBody.linearVelocity -= new Vector3(1, 0, 0);
-        Debug.Log("A");
-    }
-
+     // Update is called once per frame
+     void Update()
+     {
+         Vector3 velocity = rigidBody.linearVelocity;
+         velocity.x = moveDirection * moveSpeed;
+         rigidBody.linearVelocity = velocity;
+     }
+    
     //Shiftでジャンプ(1段)
     void Jump(InputAction.CallbackContext context)
     {
         if (jumpCount < maxJumpCount) 
-        { 
-            rigidBody.linearVelocity += new Vector3(0, 5, 0);
+        {
+            Vector3 velocity = rigidBody.linearVelocity;
+            velocity.y = jumpPower;
+            rigidBody.linearVelocity = velocity;
             jumpCount++;
             Debug.Log("Shift");
         }
