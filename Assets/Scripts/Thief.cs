8eae475d0da1dd1e23be695d23eba3a1620d1838
amattakarinntou <snkarin@nike.eonet.ne.jp>
2025-08-29 11:56:52 +0900

    横移動、ジャンプの実装
    ・横移動、ジャンプ(１段)の実装

--- Assets/Scripts/Thief.cs
+++ Assets/Scripts/Thief.cs
@@ -7,6 +7,9 @@ public class Thief : MonoBehaviour
     Rigidbody rigidBody;
     ThiefInput thiefInput;
 
+    public int maxJumpCount = 1;
+    public int jumpCount = 0;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     void Start()
@@ -14,7 +17,9 @@ public class Thief : MonoBehaviour
         hp = new Hp(3);
         rigidBody = GetComponent<Rigidbody>();
         thiefInput = new ThiefInput();
-        thiefInput.Move.MoveRight.performed += MoveRight;
+        thiefInput.Move.MoveRight.started += MoveRight;
+        thiefInput.Move.MoveLeft.started += MoveLeft;
+        thiefInput.Move.Jump.started += Jump;
         thiefInput.Enable();
     }
 
@@ -24,10 +29,35 @@ public class Thief : MonoBehaviour
        
     }
 
-
+    //Dで右移動
     void MoveRight(InputAction.CallbackContext context)
     {
         rigidBody.linearVelocity += new Vector3(1, 0, 0);
         Debug.Log("D");
     }
-}
+
+    //Aで左移動
+    void MoveLeft(InputAction.CallbackContext context) 
+    {
+        rigidBody.linearVelocity -= new Vector3(1, 0, 0);
+        Debug.Log("A");
+    }
+
+    //Shiftでジャンプ(1段)
+    void Jump(InputAction.CallbackContext context)
+    {
+        if (jumpCount < maxJumpCount) 
+        { 
+            rigidBody.linearVelocity += new Vector3(0, 5, 0);
+            jumpCount++;
+            Debug.Log("Shift");
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))  //空中にある足場にもGroundというtagをつける必要がある
+        {
+            jumpCount = 0;
+        }
+    }
+} 
