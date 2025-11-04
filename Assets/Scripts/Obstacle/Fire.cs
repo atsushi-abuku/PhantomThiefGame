94c7103c09050924798aa039476a79c9837b59b8
ouchan <kingbook0628@gmail.com>
2025-11-04 22:54:00 +0900

    炎をつくった
    Hpは３からスタート。炎に当たるとHpが１減る。無敵の間はHpが減らない。

--- Assets/Scripts/Obstacle/Fire.cs
+++ Assets/Scripts/Obstacle/Fire.cs
@@ -2,12 +2,15 @@ using UnityEngine;
 
 public class Fire : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void OnTriggerEnter(Collider collider)
     {
         if (collider.tag == "Thief")
         {
-            Debug.Log("Damage");
+            var thief = collider.GetComponentInParent<Thief>();
+            if (thief != null)
+            {
+                thief.Damage();
+            }
         }
     }
 }
--- Assets/Scripts/Thief/Thief.cs
+++ Assets/Scripts/Thief/Thief.cs
@@ -76,7 +76,7 @@ public class Thief : MonoBehaviour
             if (invincibleTimer >= invincibleDuration)
             {
                 isInvincible = false;
-                Debug.Log("–³“GI—¹");
+                Debug.Log("Im No Longer Invincible");
             }
         }
         //Œü‚«‚ÌØ‚è‘Ö‚¦
@@ -164,10 +164,10 @@ public class Thief : MonoBehaviour
     {
         if (isInvincible)
         {
-            Debug.Log("–³“G’†");
+            Debug.Log("Im Invincible");
             return;
         }
-        //HP‚ğ1Œ¸‚ç‚·
+        //HP - 1
         hp = hp.SubHp(new Hp(1));
         Debug.Log(hp.GetValue());
 
